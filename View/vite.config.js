df4b79c46d98f5cb5475ccf04777df00d5a1c6ef
Victor Bermudez <victormanuel.b.ruiz@gmail.com>
2024-04-15 14:36:41 -0400

    Se agrego un cambio

--- View/vite.config.js
+++ View/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://tic-tac-toe-rpl1.onrender.com',
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'https://tic-tac-toe-rpl1.onrender.com',
         ws: true
       }
     }
