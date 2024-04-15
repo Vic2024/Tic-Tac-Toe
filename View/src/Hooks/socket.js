9a1c6777c908ae240894f8bfa7ea7f6bba53d659
Victor Bermudez <victormanuel.b.ruiz@gmail.com>
2024-04-15 17:17:47 -0400

    Proyecto Base Tic Tac Toe

--- View/src/Hooks/socket.js
+++ View/src/Hooks/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 let socket
 
 export const initiateSocket = (user, cb) => {
-    socket = io('https://tic-tac-toe-rpl1.onrender.com', {
+    socket = io('/', {
         auth: {
             serverOffset: 0,
             isLogged: true,
