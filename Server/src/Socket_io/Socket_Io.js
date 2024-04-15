990f99556633bdfa0986f783adbcc8c061c13bf7
Victor Bermudez <victormanuel.b.ruiz@gmail.com>
2024-04-15 18:04:34 -0400

    Proyecto Base Tic Tac Toe

--- Server/src/Socket_io/Socket_Io.js
+++ Server/src/Socket_io/Socket_Io.js
@@ -11,6 +11,10 @@ import jwt from 'jsonwebtoken'
 export const app = express()
 export const server = createServer(app)
 const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  },
   connectionStateRecovery: {
     maxDisconnectionDuration: 2 * 60 * 1000,
     skipMiddlewares: true
--- View/vite.config.js
+++ View/vite.config.js
@@ -6,17 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/api': 'https://tic-tac-toe-rpl1.onrender.com',
+      /* '/socket.io': {
         target: 'https://tic-tac-toe-rpl1.onrender.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/socket.io/': {
-        target: 'https://tic-tac-toe-rpl1.onrender.com',
-        changeOrigin: true,
-        secure: false,
         ws: true,
-      }
+      } */
     }
   }
 })
